381e9a2a7eceb98ea6a2b11a49a0846f60d92124
Henrik Drefs <henrik.drefs@outlook.de>
2019-07-09 19:46:22 +0200

    export user data model

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { default as Receipt, Timestamp } from './model/Receipt';
 export { default as Address } from './model/Address';
 export { default as Time } from './model/Time';
 export { default as Amount } from './model/Amount';
+export { default as UserData } from './model/UserData';
 
 /**
  * Place type mappings
