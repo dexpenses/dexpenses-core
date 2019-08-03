7a745a60a82f0266562c91168207c9433a04518d
Henrik Drefs <henrik.drefs@outlook.de>
2019-08-04 00:18:37 +0200

    fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 /**
  * Models
  */
-export { default as Receipt, Timestamp } from './model/Receipt';
+export {
+  default as Receipt,
+  Timestamp,
+  PaymentMethod,
+  paymentMethods,
+} from './model/Receipt';
 export { default as Address } from './model/Address';
 export { default as Time } from './model/Time';
 export { default as Amount } from './model/Amount';
